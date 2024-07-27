152f4e71fa9635f605ca57dee88f102adfa34b46
Juho Choi <juho.c98@gmail.com>
2024-07-27 12:11:44 -0400

    .

--- Array/27.js
+++ Array/27.js
@@ -14,3 +14,15 @@ var removeElement = function (nums, val) {
   }
   return len;
 };
+
+var removeElement2 = function (nums, val) {
+  let len = nums.length;
+  let left = 0;
+  for (let i = 0; i < len; i++) {
+    if (nums[i] !== val) {
+      [nums[left], nums[i]] = [nums[i], nums[left]];
+      ++left;
+    }
+  }
+  return left;
+};
