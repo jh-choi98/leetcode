a02501fabd492a07690a9749cb9e0147f9da3caa
Juho Choi <juho.c98@gmail.com>
2024-07-31 03:08:04 -0400

    .

--- Array/448.js
+++ Array/448.js
@@ -1,5 +1,7 @@
 // Find All Numbers Disappeared in an Array
 
+// T: O(n)
+// S: O(n)
 var findDisappearedNumbers = function (nums) {
   const len = nums.length;
   let set = new Set();
@@ -12,4 +14,24 @@ var findDisappearedNumbers = function (nums) {
   }
 
   return [...set];
+};
+
+// T: O(n)
+// S: O(1)
+var findDisappearedNumbers2 = function (nums) {
+  let newNums = new Array();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[Math.abs(nums[i]) - 1] > 0) {
+      nums[Math.abs(nums[i]) - 1] *= -1;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      newNums.push(i + 1);
+    }
+  }
+
+  return newNums;
 };
