b171f085fe7ddbc76979e855cd5ea235d49b0d35
Juho Choi <juho.c98@gmail.com>
2024-09-07 16:51:22 -0400

    .

--- Algorithm/Recursion/700.py
+++ Algorithm/Recursion/700.py
@@ -1,1 +1,22 @@
 # Search in a BST
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# T: O(logN) in the average case, and O(N) in the worst case.
+# S: O(logN) in the average case, and O(N) in the worst case.
+class Solution:
+    def searchBST(self, root, val):
+        if not root:
+            return None
+
+        if root.val == val:
+            return root
+        elif root.val < val:
+            return self.searchBST(root.right, val)
+        else:
+            return self.searchBST(root.left, val)
