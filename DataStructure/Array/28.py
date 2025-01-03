1ad573964dca4391375e819d5b9011735a2c6c2f
Juho Choi <juho.c98@gmail.com>
2025-01-03 18:44:58 -0500

    .

--- DataStructure/Array/28.py
+++ DataStructure/Array/28.py
@@ -1,7 +1,7 @@
 # 28. Find the Index of the First Occurrence in a String
 
 
-# Solution 1
+# Solution 1: Brute Force Algorithm with While
 class Solution:
     def strStr(self, haystack: str, needle: str):
         h_index = 0
@@ -20,4 +20,13 @@ class Solution:
                 h_index -= n_index - 1
                 n_index = 0
 
+        return -1
+
+
+# Solution 2: Brute Force Algorithm with For
+class Solution2:
+    def strStr(self, haystack: str, needle: str):
+        for i in range(len(haystack) + 1 - len(needle)):
+            if haystack[i : i + len(needle)] == needle:
+                return i
         return -1
