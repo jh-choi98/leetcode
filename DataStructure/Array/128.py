64af9df2ad60b8cc7c0e50e3a9f0d71264c2a73e
Juho Choi <juho.c98@gmail.com>
2025-06-30 02:24:19 -0400

    solved 128

---
+++ DataStructure/Array/128.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+
+        max_count = 0
+        for num in nums_set:
+            if num - 1 not in nums_set:
+                cur = num
+                count = 0
+                while cur in nums_set:
+                    count += 1
+                    cur += 1
+                max_count = max(max_count, count)
+            
+        return max_count
