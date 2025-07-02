7d0560736f6e4fec30671988e6459b8f3bcabf7c
Juho Choi <juho.c98@gmail.com>
2025-07-02 17:51:10 -0400

    solved 20

---
+++ DataStructure/StackDS/20.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hm = {
+            '(': ')',
+            '{': '}',
+            '[': ']'   
+        }
+
+        st = []
+        for c in s:
+            if c not in hm:
+                if st and c == hm[st[-1]]:
+                    st.pop()
+                else:
+                    return False
+            else:
+                st.append(c)
+
+        return len(st) == 0
+
+class Solution2:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = { ")": "(", "]": "[", "}": "{"}
+        
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+                
+        return len(stack) == 0
