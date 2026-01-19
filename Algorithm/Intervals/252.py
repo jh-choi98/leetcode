b3a3380cd5a4a9847777fd96bd5653ec80f8371a
Juho Choi <juho.c98@gmail.com>
2026-01-19 08:39:01 -0500

    solved 252

---
+++ Algorithm/Intervals/252.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort(key=lambda pair:pair[0])
+
+        for i in range(1, len(intervals)):
+            if intervals[i - 1][1] > intervals[i][0]:
+                return False
+
+        return True
