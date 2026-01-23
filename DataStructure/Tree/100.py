f4819616f66e5de56fda40f5cc43017063108370
Juho Choi <juho.c98@gmail.com>
2026-01-23 14:39:50 -0500

    solved 100

--- DataStructure/Tree/100.py
+++ DataStructure/Tree/100.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Optional
 
 # Definition for a binary tree node.
@@ -6,17 +7,67 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-        
+
+# DFS
+# Time: O(n)
+# Space: O(h), where h is the height of the tree
+#   - Best: O(logn)
+#   - Worst: O(n)
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q:
             return True
-
-        if not p or not q or p.val != q.val:
-            return False
         
-        left = self.isSameTree(p.left, q.left)
-        right = self.isSameTree(p.right, q.right)
+        if not p or not q:
+            return False
 
-        return left and right
+        if p.val != q.val:
+            return False
 
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+# DFS - Iterative
+# Time: O(n)
+# Space: O(n)
+class Solution2:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        stack = [(p, q)]
+        
+        while stack:
+            node1, node2 = stack.pop()
+            
+            if not node1 and node2:
+                continue
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+            
+            stack.append((node1.left, node2.left))
+            stack.append((node1.right, node2.right))
+            
+        return True
+
+# BFS
+# Time: O(n)
+# Space: O(n)
+class Solution3:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        q1 = deque([p])
+        q2 = deque([q])
+        
+        while q1 and q2:
+            for _ in range(len(q1)):
+                node1 = q1.popleft()
+                node2 = q2.popleft()
+                
+                if node1 is None and node2 is None:
+                    continue
+                
+                if not node1 or not node2 or node1.val != node2.val:
+                    return False
+
+                q1.append(node1.left)
+                q1.append(node1.right)
+                q2.append(node2.left)
+                q2.append(node2.right)
+                
+        return True
