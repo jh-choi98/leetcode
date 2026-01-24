e0af3e3a13dfa3ea795d1702a3890f18a5f051f8
Juho Choi <juho.c98@gmail.com>
2026-01-24 17:54:51 -0500

    solved 102

--- DataStructure/Tree/102.py
+++ DataStructure/Tree/102.py
@@ -6,7 +6,10 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-        
+
+# BFS with (level by level)
+# Time: O(n)
+# Space: O(n)       
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
@@ -30,3 +33,22 @@ class Solution:
 
             results.append(cur_level)
         return results
+
+# DFS
+# Time: O(n)
+# Space: O(n)
+class Solution2:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        def dfs(node, depth):
+            if not node:
+                return None
+            if len(res) == depth:
+                res.append([])
+            
+            res[depth].append(node.val)
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+            
+        dfs(root, 0)
+        return res
