04b0af8aa4e4df9a562c2601eca11a40a0275d72
Juho Choi <juho.c98@gmail.com>
2026-01-24 17:10:52 -0500

    solved 235

--- DataStructure/Tree/235.py
+++ DataStructure/Tree/235.py
@@ -6,7 +6,12 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
-
+# Recursion
+# Time: O(h)
+# Space: O(h)
+# where h is the height of the tree
+#   - Best: O(logn)
+#   - Worst: O(n)
 class Solution:
     def lowestCommonAncestor(self, root: Optional['TreeNode'], p: 'TreeNode', q: 'TreeNode') -> Optional['TreeNode']:
         if not root or not p or not q:
@@ -30,6 +35,24 @@ class Solution2:
             return root
 
 class Solution3:
+    def lowestCommonAncestor(self, root: Optional['TreeNode'], p: TreeNode, q: TreeNode) -> Optional['TreeNode']:
+        if not root or not p or not q:
+            return None
+        
+        if max(p.val, q.val) < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif min(p.val, q.val) > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+
+# Iteration
+# Time: O(h)
+# Space: O(1)
+# where h is the height of the tree
+#   - Best: O(logn)
+#   - Worst: O(n)
+class Solution4:
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> Optional['TreeNode']:
         cur = root
         
