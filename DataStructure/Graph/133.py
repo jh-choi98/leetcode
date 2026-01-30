b214075a0e7d0b14b39afa7985012fddd17b804d
Juho Choi <juho.c98@gmail.com>
2026-01-30 14:55:47 -0500

    solved 133

--- DataStructure/Graph/133.py
+++ DataStructure/Graph/133.py
@@ -1,69 +1,51 @@
+from collections import deque
+from typing import Optional
+
 # Definition for a Node.
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-
-from collections import deque
-from typing import Optional
-
+   
+# DFS
+# Time: O(V + E)
+# Space: O(V)   
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return None
-
-        new_root = Node(node.val)
-        queue = deque([(node, new_root)])
-        visited = {node: new_root}
-
-        while queue:
-            cur_node, new_node = queue.popleft()
-            for n in cur_node.neighbors:
-                n_copy = visited.get(n)
-                if not n_copy:
-                    n_copy = Node(n.val)
-                    visited[n] = n_copy
-                    queue.append((n, n_copy))
-                new_node.neighbors.append(n_copy)
-        return new_root
-
-# BFS
-# Time complexity: O(V + E)
-# Space complexity: O(V)
-class Solution2:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not Node:
-            return None
         
-        oldToNew = {}
-        oldToNew[node] = Node(node.val)
-        queue = deque([node])
-        
-        while queue:
-            cur = queue.popleft()
-            for nei in cur.neighbors:
-                if nei not in oldToNew:
-                    oldToNew[nei] = Node(nei.val)
-                    queue.append(nei)
-                oldToNew[cur].neighbors.append(oldToNew[nei])
-                
-        return oldToNew[node]    
-
-# DFS
-# Time complexity: O(V + E)
-# Space complexity: O(V)
-class Solution3:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        oldToNew = {}
+        old_to_new = {}
         
         def dfs(node):
-            if node in oldToNew:
-                return oldToNew[node]
+            if node in old_to_new:
+                return old_to_new[node]
             
             copy = Node(node.val)
-            oldToNew[node] = copy
+            old_to_new[node] = copy
             for nei in node.neighbors:
                 copy.neighbors.append(dfs(nei))
             return copy
         
-        return dfs(node) if node else None
+        return dfs(node)
+
+# BFS
+# Time: O(V + E)
+# Space: O(V)  
+class Solution2:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        
+        old_to_new = {node: Node(node.val)}
+        queue = deque([node])
+        
+        while queue:
+            cur = queue.popleft()
+            for nei in node.neighbors:
+                if nei not in old_to_new:
+                    old_to_new[nei] = Node(nei.val)
+                    queue.append(nei)
+                old_to_new[cur].neighbors.append(old_to_new[nei])
+                
+        return old_to_new[node]
