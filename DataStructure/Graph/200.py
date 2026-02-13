1595112f2ed41306da53cfca76c018c489a2d649
Juho Choi <juho.c98@gmail.com>
2026-02-13 16:05:21 -0500

    solved 200

--- DataStructure/Graph/200.py
+++ DataStructure/Graph/200.py
@@ -1,6 +1,9 @@
 from collections import deque
+from typing import List
 
-
+# BFS
+# Time: O(m * n)
+# Space: O(m * n)
 class Solution:
     def numIslands(self, grid: list[list[str]]) -> int:
         if not grid:
@@ -27,3 +30,82 @@ class Solution:
                     count += 1
                     bfs(r, c)
         return count
+    
+# Disjoint Set Union
+# Time: O(m * n)
+# Space: O(m * n)
+class DSU:
+    def __init__(self, n):
+        self.Parent = list(range(n + 1))
+        self.Size = [1] * (n + 1)
+
+    def find(self, node):
+        if self.Parent[node] != node:
+            self.Parent[node] = self.find(self.Parent[node])
+        return self.Parent[node]
+
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+        if pu == pv:
+            return False
+
+        if self.Size[pu] < self.Size[pv]:
+            pu, pv = pv, pu
+        self.Size[pu] += self.Size[pv]
+        self.Parent[pv] = pu
+        return True
+    
+class Solution2:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        dsu = DSU(ROWS * COLS)
+
+        def index(r, c):
+            return r * COLS + c
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        count = 0
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    count += 1
+                    for dr, dc in directions:
+                        nr, nc = r + dr, c + dc
+                        if (nr < 0 or nc < 0 or nr >= ROWS or
+                            nc >= COLS or grid[nr][nc] == "0"
+                        ):
+                            continue
+                        
+                        if dsu.union(index(r, c), index(nr, nc)):
+                            count -= 1
+
+        return count
+
+# DFS
+# Time: O(m * n)
+# Space: O(m * n)
+class Solution3:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r >= ROWS or
+                c >= COLS or grid[r][c] == "0"
+            ):
+                return
+
+            grid[r][c] = "0"
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    islands += 1
+
+        return islands
